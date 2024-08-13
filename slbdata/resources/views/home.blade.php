65b5f0b2865aea1df68dc66e065f3fbf8ff89707
Alhilal Muzakkir <AMuzakkir@slb.com>
2024-08-13 16:24:14 +0700

    slb laravel data

---
+++ slbdata/resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Halaman Beranda</title>
+
+    <!--Navbar Section-->
+    <nav class="navbar">
+    </div>
+    <div class="navbar-content">
+        <a href="#home" style="font-size: 20px;">Beranda</a>
+        <a href="#services" style="font-size: 20px;">Layanan</a>
+        <a href="#about" style="font-size: 20px;">Tentang</a>
+        <a href="#contact" style="font-size: 20px;">Kontak</a>
+    </div>
+    <div class="dropdown">
+            <button class="login-button">Akun</button>
+            <div class="dropdown-content">
+                <a href="#">Logout</a>
+                <a href="#">Profile</a>
+                <a href="#">Settings</a>
+                <a href="#">Activity</a>
+            </div>
+</nav>
+</head>
+ <div class="main-content" style="margin-top: 10px;"> <!-- Menambahkan jarak antara navbar dan content -->
+ <div class="content-wrapper">
+            <div class="activity-grid">
+                <div class="activity-box animated">
+                    <div class="activity-icon">
+                        <i class="fas fa-chart-line"></i>
+                    </div>
+                    <div class="activity-content">
+                        <h3 class="activity-title">Good Morning Hilal</h3>
+                        
+                    </div>
+                </div>
+            </div>
---
+++ slbdata/resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Welcome SLB Database</title>
+</head>
+<body>
+    <div class="main-content">
+        <div class="header">Organize your data with <span>SLB Data</span></div>
+        <p>Dengan SLB Data, Anda dapat mengelola data Anda dengan lebih mudah dan efisien.</p>
+    </div>
+    <div class="image-container">
+        <img src="img/homeback.png" alt="Kelola Data Anda" class="animated">
+    </div>
+    <!-- Section About App -->
+    <div class="main-content">
+        <div class="header">About <span>Application</span></div>
+        <p>SLB Data App is an information management solution designed to simplify data management with easy-to-use features, including the ability to upload, add and search data. The data upload feature allows users to import information from various file formats such as CSV directly into the system, with an intuitive interface to map data fields and ensure seamless integration. The add data feature facilitates manual input of information through forms designed to enter various types of data, including text, numbers, and dates, as well as include notes or attachments. Meanwhile, the search data feature makes it easy to find specific information in the database, using keywords, filters, and search parameters to filter results and quickly obtain the desired data. With these three main features, SLB Data offers convenience and efficiency in managing and accessing information, making it a very useful tool for a variety of data management needs.</p>
+    </div>
+</body>
+</html>
